73de2f1a4160018cf94830b8cbf94a33e000b402
sanday08 <sandip.job89@gmail.com>
2020-04-18 23:31:03 +0530

    Updates

---
+++ precache-manifest.5a3cc3a5194339f3e51031a37753f6aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953f1f220f32efb7e4b7186349814058",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "0c2741112d299d31e36c",
+    "url": "/React-ToDo/static/js/2.86973a85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo/static/js/2.86973a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5c89def5ee7d26375c",
+    "url": "/React-ToDo/static/js/main.84c8c72c.chunk.js"
+  },
+  {
+    "revision": "83da9c4cb3fdee3dc721",
+    "url": "/React-ToDo/static/js/runtime-main.9e27e800.js"
+  }
+]);
